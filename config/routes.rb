663606c48282afe4302c7da46c2bd576d8b9cc31
NATALIA ESPAÑA <natacski@gmail.com>
2020-11-24 14:17:42 -0500

    backend-2-lesson-1 (new additions)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments  #will replace the code below
+    # get    '/comments',          to: 'comments#index', as: 'comments'
+    # post   '/comments',          to: 'comments#create'
+    # get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
+    # get    '/comments/:id',      to: 'comments#show',  as: 'comment'
+    # get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
+    # patch  '/comments/:id',      to: 'comments#update'
+    # delete '/comments/:id',      to: 'comments#destroy'
+  end
   resources :pages
+
+  
   #with resources :pages we can replace all the following routes
   # get '/pages', to: 'pages#index'
   # post '/pages', to: 'pages#create'
